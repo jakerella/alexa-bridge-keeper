4f6a19e435fbb1b79497250908210c5fa0eef355
Jordan Kasper <jordan@jordankasper.com>
2016-07-30 15:05:29 -0400

    Fixed some Alexa request verification errors

--- app/verify.js
+++ app/verify.js
@@ -3,13 +3,15 @@
 let verifier = require('alexa-verifier');
 
 module.exports = function(req, res, next) {
-    if (!req.headers.signaturecertchainurl) {
-        return next();
+    if (!req.body.session || !req.body.request || !req.headers.signaturecertchainurl) {
+        console.error(req.body, req.headers.signaturecertchainurl);
+        return res.status(400).json({ message: 'Invalid Request Body', error: null });
     }
+
     verifier(req.headers.signaturecertchainurl, req.headers.signature, req.rawBody, function(err) {
         if (err) {
             console.error(err);
-            res.status(401).json({ status: 'Verification Failure', reason: err.message });
+            res.status(401).json({ message: 'Verification Failure', error: err });
         } else {
             next();
         }
